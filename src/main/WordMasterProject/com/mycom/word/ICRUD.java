77a45d03f2cac4f0bae730c239b0044abc145843
LeeMineo <lms4882@gmail.com>
2022-09-01 21:18:08 +0900

    Update Project

---
+++ src/main/WordMasterProject/com/mycom/word/ICRUD.java
@@ -0,0 +1,10 @@
+package com.mycom.word;
+
+public interface ICRUD { //인터페이스는 국룰로 I를 붙임
+    public Object add(); // 데이터 추가할때 사용하는 함수
+    // 추가된 객체를 리턴해주기 위해 오브젝트를 사용
+    public int update(Object obj); // 데이터 수정할때 사용하는 함수
+    public int delete(Object obj);// 데이터 삭제할때 사용하는 함수
+    public void selectOne(int id);// 데이터 한개를 조회할때 사용하는 함수
+    //여기에 들어가는 리턴타입이나 파라미터는 작성하는 사람이 원하는 템플릿을 구성하는것에 맞추어서 작성하면 된다.
+}
---
+++ src/main/WordMasterProject/com/mycom/word/WordManager.java
@@ -0,0 +1,8 @@
+package com.mycom.word;
+
+public class WordManager { //Word CRUD를 이용해서 구현 하는 전체적인 관리 역할
+
+    public void start() {
+        System.out.println("WordManager!!!!");
+    }
+}
