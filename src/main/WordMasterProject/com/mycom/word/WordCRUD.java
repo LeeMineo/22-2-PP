4e14cfd09442c9ef62d9772baedceef63fa37fcb
LeeMineo <lms4882@gmail.com>
2022-09-02 14:44:13 +0900

    Update Project

--- src/main/WordMasterProject/com/mycom/word/WordCRUD.java
+++ src/main/WordMasterProject/com/mycom/word/WordCRUD.java
@@ -1,9 +1,40 @@
 package com.mycom.word;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class WordCRUD implements ICRUD{
+    ArrayList<Word> list;
+    Scanner s;
+    WordCRUD(Scanner s){
+        list = new ArrayList<>();
+        this.s =s;
+    }
+
+    /*
+    => 난이도 (1,2,3) & 새 단어 입력 :1 drivemay
+    뜻 입력: 차고 진입로
+    새 단어가 단어장에 추가되었습니다.
+     */
     @Override
     public Object add() {
-        return null;
+        System.out.print("=> 난이도 (1,2,3) & 새 단어 입력 : ");
+        //1 drivemays
+        int level = s.nextInt();
+        String word = s.nextLine();
+
+        System.out.print("뜻 입력 : ");
+        //차고 진입로
+        String meaning = s.nextLine();
+
+        return new Word(0, level, word, meaning);
+    }
+
+    public void addWord(){
+        Word one = (Word)add();
+        list.add(one);
+        System.out.println("새 단어가 단어장에 추가되었습니다.");
+
     }
 
     @Override
